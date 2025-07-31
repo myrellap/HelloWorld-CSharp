39a41346683f8223165d20ea2e7c6bc397139d57
myrellap <myrella.putti@aluno.senai.br>
2025-07-31 10:33:30 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var numero1 = 2;
+        var numero2 = 3;
+
+        var soma = numero1 + numero2;
+
+        Console.WriteLine("Bem vindo ao mundo de programação C#");
+        Console.WriteLine("Olá, myrella!");
+
+        Console.WriteLine("A soma e igual a:" + soma);
+    }
+}
